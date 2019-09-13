ee2b025e043a7c065e1a82dd929c6aa4cc423d8d
Jace Benson <jace.benson@protonmail.com>
2019-09-13 10:16:21 -0500

    added spacing

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,92 +1,92 @@
 /* global $, mermaid */
-$(document).ready(function () {
-  var GLOBAL = window;
-  GLOBAL.cb = function (svgGraph) {
-    $('#chart').append('<div class="mermaid"></div>');
-    $('.mermaid').html(svgGraph);
-  };
-  mermaid.ganttConfig = {
-    axisFormatter: [
-      ['%-m/%-d', function (d) {
-        return d.getDay() === 1;
-      }]
-    ]
-  };
-  GLOBAL.renderLive = function () {
-    if ($('#live').val() === 'yes') {
-      GLOBAL.render();
-    }
-  };
-  GLOBAL.render = function () {
-    $('#chart').html('');
-    GLOBAL.mermaidSyntax = $('#mermaid-syntax').val();
-    // GLOBAL.mermaidSyntax =
-    var matches = GLOBAL.mermaidSyntax.match(/\n/g);
-    var breaks = matches ? matches.length : 2;
-    $('#mermaid-syntax').attr('rows', breaks + 2);
-    if (mermaid.parse(GLOBAL.mermaidSyntax)) {
-      $('#info').text('');
-      mermaid.render('mermaid', GLOBAL.mermaidSyntax, GLOBAL.cb);
-    }
-  };
-  mermaid.parseError = function (err, hash) {
-    var error = err.replace(/\n/g, '<br/>').split('^')[0] + '^';
-    $('#info').html(error);
-  };
+$(document).ready(function() {
+    var GLOBAL = window;
+    GLOBAL.cb = function(svgGraph) {
+        $('#chart').append('<div class="mermaid"></div>');
+        $('.mermaid').html(svgGraph);
+    };
+    mermaid.ganttConfig = {
+        axisFormatter: [
+            ['%-m/%-d', function(d) {
+                return d.getDay() === 1;
+            }]
+        ]
+    };
+    GLOBAL.renderLive = function() {
+        if ($('#live').val() === 'yes') {
+            GLOBAL.render();
+        }
+    };
+    GLOBAL.render = function() {
+        $('#chart').html('');
+        GLOBAL.mermaidSyntax = $('#mermaid-syntax').val();
+        // GLOBAL.mermaidSyntax =
+        var matches = GLOBAL.mermaidSyntax.match(/\n/g);
+        var breaks = matches ? matches.length : 2;
+        $('#mermaid-syntax').attr('rows', breaks + 2);
+        if (mermaid.parse(GLOBAL.mermaidSyntax)) {
+            $('#info').text('');
+            mermaid.render('mermaid', GLOBAL.mermaidSyntax, GLOBAL.cb);
+        }
+    };
+    mermaid.parseError = function(err, hash) {
+        var error = err.replace(/\n/g, '<br/>').split('^')[0] + '^';
+        $('#info').html(error);
+    };
 
-  // onkeyup rerender
-  $('#mermaid-syntax').keyup(function () {
-    GLOBAL.renderLive();
-  });
-
-  $('#type').change(function () {
-    var flow = $('#type').val().split('/')[1].split('.mmd')[0];
-    console.log(flow);
-    window.history.pushState('page2', 'Title', GLOBAL.replaceQueryParam('flow', flow, window.location.search));
-    $.get($('#type').val(), function (data) {
-      $('#mermaid-syntax').val(data + '\n');
-      GLOBAL.render();
+    // onkeyup rerender
+    $('#mermaid-syntax').keyup(function() {
+        GLOBAL.renderLive();
     });
-    // $('#mermaid-syntax').val(example[$('#type').val()]);
-  });
 
-  // force update
-  $('#update').click(function () {
-    GLOBAL.render();
-  });
-
-  // resize
-  $('#resize').click(function () {
-    $('#left').toggleClass('col-6');
-    $('#left').toggleClass('d-none');
-    $('#right').toggleClass('col-6');
-    $('#right').toggleClass('col-12');
-  });
-  GLOBAL.getUrlParameter = function getUrlParameter (sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1));
-    var sURLVariables = sPageURL.split('&');
-    var sParameterName;
-
-    for (var i = 0; i < sURLVariables.length; i++) {
-      sParameterName = sURLVariables[i].split('=');
-
-      if (sParameterName[0] === sParam) {
-        return sParameterName[1] === undefined ? true : sParameterName[1];
-      }
-    }
-  };
-  GLOBAL.replaceQueryParam = function (param, newval, search) {
-    var regex = new RegExp('([?;&])' + param + '[^&;]*[;&]?');
-    var query = search.replace(regex, '$1').replace(/&$/, '');
-
-    return (query.length > 2 ? query + '&' : '?') + (newval ? param + '=' + newval : '');
-  };
-  if (GLOBAL.getUrlParameter('flow')) {
-    $('#type').val('mmd/' + GLOBAL.getUrlParameter('flow') + '.mmd');
-    $.get($('#type').val(), function (data) {
-      $('#mermaid-syntax').val(data + '\n');
-      GLOBAL.render();
+    $('#type').change(function() {
+        var flow = $('#type').val().split('/')[1].split('.mmd')[0];
+        console.log(flow);
+        window.history.pushState('page2', 'Title', GLOBAL.replaceQueryParam('flow', flow, window.location.search));
+        $.get($('#type').val(), function(data) {
+            $('#mermaid-syntax').val(data + '\n');
+            GLOBAL.render();
+        });
+        // $('#mermaid-syntax').val(example[$('#type').val()]);
     });
-  }
+
+    // force update
+    $('#update').click(function() {
+        GLOBAL.render();
+    });
+
+    // resize
+    $('#resize').click(function() {
+        $('#left').toggleClass('col-6');
+        $('#left').toggleClass('d-none');
+        $('#right').toggleClass('col-6');
+        $('#right').toggleClass('col-12');
+    });
+    GLOBAL.getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1));
+        var sURLVariables = sPageURL.split('&');
+        var sParameterName;
+
+        for (var i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+    GLOBAL.replaceQueryParam = function(param, newval, search) {
+        var regex = new RegExp('([?;&])' + param + '[^&;]*[;&]?');
+        var query = search.replace(regex, '$1').replace(/&$/, '');
+
+        return (query.length > 2 ? query + '&' : '?') + (newval ? param + '=' + newval : '');
+    };
+    if (GLOBAL.getUrlParameter('flow')) {
+        $('#type').val('mmd/' + GLOBAL.getUrlParameter('flow') + '.mmd');
+        $.get($('#type').val(), function(data) {
+            $('#mermaid-syntax').val(data + '\n');
+            GLOBAL.render();
+        });
+    }
 
 });
